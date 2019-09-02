baa6121aa6de2874dd215476dc20f789705284ca
Jomon Johnson <jomon.entero@gmail.com>
2019-09-02 19:47:28 +0530

    Update de-4.sh

--- versions/de-4.sh
+++ versions/de-4.sh
@@ -3,7 +3,7 @@
 mkdir ~/.codexten
 mkdir ~/.codexten/bin
 
-set_env `PATH` `$HOME/.codexten/bin:$PATH`
+set_env 'PATH' '$HOME/.codexten/bin:$PATH'
 
 ln -s ~/.de/bin/de ~/.codexten/bin/de
 ln -s ~/.de/bin/de ~/.codexten/bin/dev
