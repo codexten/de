aa598921419aa513cfb7ec84d63dd7144d3ee282
Jomon Johnson <jomon.entero@gmail.com>
2019-09-02 14:47:16 +0530

    init

---
+++ versions/de-2.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# Removing Existin cron
+croncmd="cd $deDir && ./bin/update >> $runtimeDir/cron.log 2>&1"
+cronjob="* * * * * $croncmd"
+crontab -l
+
+
+# adding new cron for every 1 hrs
+( crontab -l | grep -v -F "$croncmd" ) | crontab -
+croncmd="cd $deDir && ./bin/update > $runtimeDir/cron.log 2>&1"
+cronjob="0 * * * * $croncmd"
+
+( crontab -l | grep -v -F "$croncmd" ) | crontab -
+( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
+
+crontab -l
