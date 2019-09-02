9392a73876d02721ffe19e339975c9e597e1466a
Jomon Johnson <jomon.entero@gmail.com>
2019-09-02 18:45:03 +0530

    init

---
+++ bin/_de/docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+case $2 in
+   "dev-panel")
+      git clone https://github.com/codexten/dev-panel.git ~/.dev-panel
+      chmod +x ~/.dev-panel/bin/install.sh
+      ~/.dev-panel/bin/install.sh
+      ;;
+    *)
+     echo "Invalid Install Command"
+     ;;
+esac
